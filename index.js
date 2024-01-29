a6e583642945e34fedba9bc29fb3d8fa22dafdb3
jorwi jesus <jgjg.0503@gmail.com>
2024-01-29 09:07:46 -0400

    portMod

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use(cors());
 app.use('/', routes);
 const PORT=process.env.PORT ?? 4000
 // Iniciar el servidor
-app.listen(4000, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
